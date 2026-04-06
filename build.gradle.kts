072b3765470185ffad2c605e9875f2c77fd45fd5
Leclowndu93150 <xdr.a123454321@gmail.com>
2026-04-06 10:30:39 +0200

    0.3.2: Fix module parent project directory resolution

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "dev.prism"
-version = "0.3.1"
+version = "0.3.2"
 
 repositories {
     mavenCentral()
--- src/main/kotlin/dev/prism/gradle/dsl/PrismSettingsExtension.kt
+++ src/main/kotlin/dev/prism/gradle/dsl/PrismSettingsExtension.kt
@@ -76,38 +76,32 @@ class PrismSettingsExtension(private val settings: Settings) {
         }
     }
 
+    private fun ensureModuleParent(moduleName: String) {
+        val modulePath = ":$moduleName"
+        settings.include(modulePath)
+        settings.project(modulePath).projectDir = File(settings.settingsDir, "modules/$moduleName")
+    }
+
     private fun registerModuleSingleProject(moduleName: String, mcVersion: String) {
         val path = ":$moduleName:$mcVersion"
         if (settings.findProject(path) != null) return
 
+        ensureModuleParent(moduleName)
         settings.include(path)
         settings.project(path).projectDir = File(settings.settingsDir, "modules/$moduleName/versions/$mcVersion")
-
-        val parentPath = ":$moduleName"
-        if (settings.findProject(parentPath) == null) {
-            settings.include(parentPath)
-            settings.project(parentPath).projectDir = File(settings.settingsDir, "modules/$moduleName")
-        }
     }
 
     private fun registerModuleSubproject(moduleName: String, mcVersion: String, loader: String) {
         val path = ":$moduleName:$mcVersion:$loader"
         if (settings.findProject(path) != null) return
 
+        ensureModuleParent(moduleName)
         settings.include(path)
         settings.project(path).projectDir = File(settings.settingsDir, "modules/$moduleName/versions/$mcVersion/$loader")
 
         val versionPath = ":$moduleName:$mcVersion"
-        if (settings.findProject(versionPath) == null) {
-            settings.include(versionPath)
-            settings.project(versionPath).projectDir = File(settings.settingsDir, "modules/$moduleName/versions/$mcVersion")
-        }
-
-        val modulePath = ":$moduleName"
-        if (settings.findProject(modulePath) == null) {
-            settings.include(modulePath)
-            settings.project(modulePath).projectDir = File(settings.settingsDir, "modules/$moduleName")
-        }
+        settings.include(versionPath)
+        settings.project(versionPath).projectDir = File(settings.settingsDir, "modules/$moduleName/versions/$mcVersion")
     }
 }
 
